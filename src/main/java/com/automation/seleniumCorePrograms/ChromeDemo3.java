3510c4684e7e2f879b06e9d0a5c949ef23f26db1
neelimakari <neelima.karu@gmail.com>
2022-06-07 17:00:41 +0100

    Initial Version- Added Hashset

--- src/main/java/com/automation/seleniumCorePrograms/ChromeDemo3.java
+++ src/main/java/com/automation/seleniumCorePrograms/ChromeDemo3.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class ChromeDemo3
 {
     public static void main(String[] args) throws InterruptedException {
@@ -13,14 +15,14 @@ public class ChromeDemo3
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://www.gmail.com");
-        /*String actualTitle=driver.getCurrentUrl();
+        String actualTitle=driver.getCurrentUrl();
         System.out.println("Title is:" + actualTitle);
         String actualCurUrl=driver.getCurrentUrl();
         System.out.println("Current Url is:" + actualCurUrl);
         String curWindowName=driver.getWindowHandle();
         System.out.println("Current window name is:" + curWindowName);
         Set<String> windows=driver.getWindowHandles();
-        System.out.println(" window are:" + windows);*/
+        System.out.println(" window are:" + windows);
         WebElement userName=driver.findElement(By.id("identifierId"));
         userName.sendKeys("neelima.karu@gmail.com");
         WebElement nxt= driver.findElement(By.id("identifierNext"));
